a158e70ddcffc9b58bf33894e67e626cbce9f1a7
DavidSall1 <90263182+DavidSall1@users.noreply.github.com>
2024-05-10 12:15:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+import git 
+  
+# Clone a remote repository 
+repo_url = "https://github.com/Hardik-Kushwaha/GIT_Python_Automation"
+local_path = "/home/hardik/GFG_Temp/Cloned_Repo"
+repo = git.Repo.clone_from(repo_url, local_path) 
+print(f'Repository Cloned at location: {local_path}') 
+
 print("hello world")
 with open("test.txt", "w") as f:
   f.write("Hello world")
