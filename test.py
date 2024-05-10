ca89c98c5b1c5315c2362e9bc5bbd010dfb19d99
DavidSall1 <90263182+DavidSall1@users.noreply.github.com>
2024-05-10 12:47:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,6 @@
-import git 
-  
-# Clone a remote repository 
-username = "DavidSall1"
-password = "mickeodlan123"
-repo_url = "https://DavidSall1:mickeodlan123@github.com/DavidSall1/test_actions.git"
-local_path = "clone_repo"
-repo = git.Repo.clone_from(repo_url, local_path) 
-print(f'Repository Cloned at location: {local_path}') 
-
 file1 = "test.txt"
-
 print("hello world")
 with open(file1, "w") as f:
   f.write("Hello world")
 with open(file1, "r") as f:
   print(f.read())
-
-repo.index.add([file1]) 
-print('Files Added Successfully') 
-repo.index.commit('Initial commit on new branch') 
-print('Commited successfully')
