5c08061e5887a4357699ead82d04fd0775be8f6f
DavidSall1 <90263182+DavidSall1@users.noreply.github.com>
2024-05-10 12:26:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import git
   
 # Clone a remote repository 
 username = "DavidSall1"
-password = "Mickeodlan123"
+password = "mickeodlan123"
 repo_url = f"https://{username}:{password}@github.com/{username}/project.git"
 local_path = "clone_repo"
 repo = git.Repo.clone_from(repo_url, local_path) 
